bc64267d042930ec5de473a081b638c2d41301dd
readians <domenicostringless@gmail.com>
2015-01-08 21:32:43 +0100

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Forum Parthenope</title>
+<link href="tabs.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<div class="logo">Welcome, students.</div>
+    <div align="center">
+	<ul class="menu">
+    <li><a href="#" title="home" class="current"><span>Home</span></a></li>
+    <li><a href="#" title="messages"><span>Messages</span></a></li>
+    <li><a href="#" title="info"><span>Info</span></a></li>
+    <li><a href="#" title="adv"><span>Adv</span></a></li>
+    <li><a href="#" title="sub"><span>Subjects</span></a></li>
+    <li><a href="#" title="spotted"><span>Spotted</span></a></li>  
+	</ul>
+    </div>
+	<div class="content">
+	</div>
+</body>
+</html>
